833afb811835a6f96fe5d62c04bab7d297b18376
vpadegaonkar <vpadegaonkar@yahoo.com>
2016-10-08 17:48:31 +0000

    second change

--- VinayHelloWorld/src/VinayHelloWorld.java
+++ VinayHelloWorld/src/VinayHelloWorld.java
@@ -4,6 +4,7 @@ public class VinayHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello World Amazon");
+System.out.println("Second line");
 	}
 
 }
